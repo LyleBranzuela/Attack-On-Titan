ce9c3bab9d441be613d3ca8330b5ea158f505d7b
YoonkunLee <ydx9474@aut.ac.nz>
2019-08-17 11:30:10 +1200

    x

---
+++ StickWars/Assets/Sound/soundEffect.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class soundEffect : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public AudioSource block_sound;
+    public AudioSource click_sound;
+    public AudioSource wire_sound;
+    public AudioSource slash;
+
+    public void PlayBlock_sound()
+    {
+        block_sound.Play();
+    }
+
+    public void PlayClick_sound()
+    {
+        click_sound.Play();
+    }
+
+}
