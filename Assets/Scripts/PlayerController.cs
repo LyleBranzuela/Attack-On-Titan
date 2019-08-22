29d6f169c4a2f1621084599ead0ffa5a0e7ae39c
Lyle Branzuela <lylebranzuela@gmail.com>
2019-08-22 22:50:05 +1200

    Merged Johnny's updates to mine. Johnny: Added manual controls to Levi, and added some bone animation. Lyle: Fixed merge errors, and cleaned up the file system. Changed the main camera and canvas to proper values.
    Signed-off-by: Lyle Branzuela <lylebranzuela@gmail.com>

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float speed = 5f;
+    private float movement = 0f;
+    private Rigidbody2D rigidBody;
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement = Input.GetAxis("Horizontal");
+        // Debug.Log(movement);
+        if (movement > 0f)
+        {
+            rigidBody.velocity = new Vector2(movement * speed, rigidBody.velocity.y);
+        }
+        else if (movement < 0f)
+        {
+            rigidBody.velocity = new Vector2(movement * speed, rigidBody.velocity.y);
+        }
+    }
+}
