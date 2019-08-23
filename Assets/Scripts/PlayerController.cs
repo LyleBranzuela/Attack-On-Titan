6a098ee811137d7ce7f19322521eafd58fa6674e
Johnny Li <dqt4431@autuni.ac.nz>
2019-08-23 13:59:54 +1200

    Added jump function codes to playerController
    Levi can jump now , but if you continuesly press space , it can fly!!!

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,27 +4,38 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
     public float speed = 5f;
+    public float jumpSpeed = 5f;
     private float movement = 0f;
     private Rigidbody2D rigidBody;
+
+    //Use this for initialization
     void Start()
     {
         rigidBody = GetComponent<Rigidbody2D>();
     }
-
-    // Update is called once per frame
+    
+    //Update is called once per frame
     void Update()
     {
         movement = Input.GetAxis("Horizontal");
         // Debug.Log(movement);
-        if (movement > 0f)
+        if (movement > 0f) //if the movement is forward
         {
             rigidBody.velocity = new Vector2(movement * speed, rigidBody.velocity.y);
         }
-        else if (movement < 0f)
+        else if (movement < 0f) // go backward
         {
             rigidBody.velocity = new Vector2(movement * speed, rigidBody.velocity.y);
+        }
+        else //stay idle
+        {
+            rigidBody.velocity = new Vector2(0, rigidBody.velocity.y);
+        }
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            rigidBody.velocity = new Vector2(rigidBody.velocity.x, jumpSpeed);
         }
     }
 }
