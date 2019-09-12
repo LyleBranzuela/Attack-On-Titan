e431016087ed4e0ecde793cedba5ec7d57f4fbf9
Jason Chung <crd1951@aut.ac.nz>
2019-09-12 21:11:11 +1200

    Add Start runing effect
    Add Start runing effect and a script to control it.

---
+++ Assets/Scripts/RuningDustEffectControl.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RuningDustEffectControl : MonoBehaviour
+{
+    private ParticleSystem ps;
+    private float old_pos;
+    int playTime;
+    // Start is called before the first frame update
+    void Start()
+    {
+      old_pos = transform.position.x;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      ps = GetComponent<ParticleSystem>();
+
+      //Check if the boot has moved more than have frame. If yes, turn on the effect.
+      if(Input.GetKeyDown(KeyCode.RightArrow)||Input.GetKeyDown(KeyCode.LeftArrow))
+      {
+          ps.Play(); 
+      }
+      old_pos = transform.position.x;
+    }
+}
