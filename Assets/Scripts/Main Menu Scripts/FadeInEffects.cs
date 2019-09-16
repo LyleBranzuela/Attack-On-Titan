37805db20d3d770ed09a487d902044db62d7dbbe
YoonkunLee <ydx9474@aut.ac.nz>
2019-09-16 16:20:35 +1200

    Main Menu, Sound, Settings, and UI Updates.
    Signed-off-by: YoonkunLee <ydx9474@aut.ac.nz>

---
+++ Assets/Scripts/Main Menu Scripts/FadeInEffects.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FadeInEffects : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Image fade;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        fade.canvasRenderer.SetAlpha(0.0f);
+        fadeIn();
+    }
+
+    // Update is called once per frame
+    void fadeIn()
+    {
+        fade.CrossFadeAlpha(1, 2, false);
+    }
+}
---
+++ Assets/Scripts/Main Menu Scripts/sound.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sound : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public AudioSource block;
+    public AudioSource click;
+    public AudioSource wire;
+    public AudioSource slash;
+    public AudioSource cannon;
+    public AudioSource gun;
+    public AudioSource victory;
+    public AudioSource lose;
+    public AudioSource normal_stage;
+    public AudioSource boss_stage;
+    public AudioSource arrow;
+    public AudioSource musicsource;
+    public AudioSource backGround;
+
+    public void PlayBlock_sound()
+    {
+        block.Play();
+    }
+
+    public void PlayClick_sound()
+    {
+        click.Play();
+    }
+    public void PlayWire_sound()
+    {
+        wire.Play();
+    }
+    public void PlaySlash_Sound()
+    {
+        slash.Play();
+    }
+    public void PlayCannon_Sound()
+    {
+        cannon.Play();
+    }
+    public void PlayGun_Sound()
+    {
+        gun.Play();
+    }
+    public void PlayVictory_Sound()
+    {
+        victory.Play();
+    }
+    public void PlayLose_Sound()
+    {
+        lose.Play();
+    }
+    public void PlayNormalStage_Sound()
+    {
+        normal_stage.Play();
+    }
+    public void PlayBossStage_Sound()
+    {
+        boss_stage.Play();
+    }
+    public void PlayArrow_Sound()
+    {
+        arrow.Play();
+    }
+    public void PlayBackGround_sound()
+    {
+        backGround.Play();
+    }
+
+    public void SetMusicVolume(float volume)
+    {
+        musicsource.volume = volume;
+    }
+}
