86e9adfe3ce19b8606246f8fc5e1d9f679c3d48a
YoonkunLee <ydx9474@aut.ac.nz>
2019-09-19 14:05:36 +1200

    Updated Master volume and Change Scene Signed-off-by: YoonkunLee <ydx9474@aut.ac.nz>

---
+++ Assets/Scenes/SceneChange.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneChange : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Awake()
+    {
+        
+    }
+
+    public void StartButton()
+    {
+        Invoke("startGame", .5f);
+    }
+
+    void startGame()
+    {
+        Application.LoadLevel("GameScene");
+    }
+
+    public void backToMainButton()
+    {
+        Invoke("backToMain", .5f);
+    }
+    void backToMain()
+    {
+        Application.LoadLevel("MainMenu");
+    }
+}
